4796376be0ff0d9d24abef613de8ae4d88895a02
Eugeny Goryachev <gorcom2012@gmail.com>
2022-08-18 22:24:08 +0400

    Дописал оболочку до конца, но она не работает, из-за Unix-интерфейса для функций работы с процессами и каталогами А так же компилятор выдает ошибку при использовании ссылок на функции

--- Shell.c
+++ Shell.c
@@ -1,3 +1,4 @@
+// Командный интерпретатор Shell
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,9 +7,27 @@
 #define LSH_TOK_BUFSIZE 64  // Размер буфера с указателями на токены
 #define LSH_TOK_DELIM " \t\r\n\a" // Разделители для токенизации
 
+// Массив имен встроенных команд
+char *builtin_str[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+// Массив указателей на функции встроенных команд
+int (*builtin_func[]) (char **) = {
+	&lsh_cd,
+	&lsh_help,
+	&lsh_exit
+};
+
 void lsh_loop(void);
 char *lsh_read_line(void);
 char **lsh_split_line(char *line);
+int lsh_launch(char **args);
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
 
 
 int main (int argc, char **argv) { // Поправить аргументы char
@@ -125,3 +144,74 @@ char **lsh_split_line(char *line) {
 	tokens[position] = NULL;
 	return tokens;
 }
+
+
+// Запуск процессов
+int lsh_launch(char **args) {
+	
+}
+
+
+//
+int lsh_num_builtins() {
+		return sizeof(builtin_str) / sizeof(char *);
+}
+
+
+// Команда смены каталога
+int lsh_cd(char **args) {
+	// Проверяем, есть ли второй аргумент
+	if (args[1] == NULL) {
+		// Сообщение об ошибке
+		fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+	}
+	else {
+		// Вызываем функцию смены каталога
+		//if (chdir(args[1]) != 0) {
+		//	perror("lsh");
+		//}
+	}
+	return 1;
+}
+
+
+// Команда help
+int lsh_help(char **args) {
+	int i;
+	printf("Gor.Com Shell\n");
+	printf("Type program names and arguments, and hit enter.\n");
+	printf("The following are built in:\n");
+	
+	// Выводим массив имен встроенных команд
+	for (i = 0; i < lsh_num_builtins(); i++) {
+		printf(" %s\n", builtin_str[i]);
+	}
+	printf("Use the man command for information on other programs.\n");
+	return 1;
+}
+
+
+// Команда выход
+int lsh_exit(char **args) {
+	return 0;
+}
+
+
+// Выполнение встроенных команд или запуск процессов
+int lsh_execute(char **args) {
+	int i;
+	
+	// Проверяем, не путая ли строка введена
+	if (args[0] == NULL) {
+		return 1;
+	}
+	
+	for (i = 0; i < lsh_num_builtins(); i++) {
+		if (strcmp(args[0], builtin_str[i]) == 0) {
+			return (*builtin_func[i])(args);
+		}
+	}
+	
+	return lsh_launch(args);
+}
+
